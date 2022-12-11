a225acd3eb3504843db720d4ba51f01d97dc80ef
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2022-12-11 18:09:11 -0300

    refactor: customer schema regex update

--- src/models/customers.schema.js
+++ src/models/customers.schema.js
@@ -2,7 +2,13 @@ import joi from "joi";
 
 export const customerSchema = joi.object({
   name: joi.string().max(40).required(),
-  phone: joi.string().min(10).max(11).regex(/^\d+$/),
-  cpf: joi.string().length(11).regex(/^\d+$/),
-  birthday: joi.date(),
+  phone: joi.string().min(10).max(11).regex(/^\d+$/).required(),
+  cpf: joi.string().length(11).regex(/^\d+$/).required(),
+  birthday: joi
+    .string()
+    .regex(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)
+    .message({
+      "string.pattern.base": "birthday must match the pattern YYYY-MM-DD",
+    })
+    .required(),
 });
